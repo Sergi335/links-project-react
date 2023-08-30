b4c1ec1bc04893582261c063ed118a929463ac18
Sergio <sergiadn335@gmail.com>
2023-08-30 06:07:40 +0200

    Clean app componet

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,23 @@ import { useSessionStore } from '../src/store/session'
 
 function App () {
   const user = useSessionStore(state => state.user)
-  const getData = () => {
-    // console.log('Desde el App')
-    const data = 'Holaaa'
-    return data
-  }
   const router = createBrowserRouter([
     {
       path: '/',
-      loader: getData,
       element: <HomePage />
     },
     {
       path: '/desktop/:desktopName',
-      loader: getData,
       element: user === null ? <Navigate to="/" replace={true} /> : <AppLayout />
     },
     {
       path: '/login',
-      loader: getData,
       element: user === null ? <Login /> : <Navigate to="/desktop/inicio" replace={true} />
     }
   ])
 
   return (
     <RouterProvider router={router}>
-      {/* <Routes>
-        <Route path='/' element={<HomePage />}/>
-        <Route path='/desktop/:name' element={<Main />} loader={getData} />
-      </Routes> */}
     </RouterProvider>
   )
 }
