565340ad38f6184cb7bb98425fd363bc747a8874
Sergio <sergiadn@hotmail.com>
2024-12-24 03:51:04 +0100

    fixed tanstack query version added useLinks hook

--- src/hooks/useLinks.js
+++ src/hooks/useLinks.js
@@ -1,3 +1,26 @@
+import { useReducer } from 'react'
 import { useGlobalStore } from '../store/global'
 
 const initialState = useGlobalStore(state => state.globalLinks)
+
+function reducer (state = initialState, action) {
+  switch (action.type) {
+    case 'add':
+      return [...state, action.payload]
+    case 'remove':
+      return state.filter(link => link._id !== action.payload)
+    case 'update':
+      return state.map(link => link._id === action.payload._id ? action.payload : link)
+    default:
+      return state
+  }
+}
+
+export function useLinks () {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return {
+    state,
+    dispatch
+  }
+}
